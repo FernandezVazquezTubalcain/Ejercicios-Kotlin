39f6a499a0f20c707ef3ad2f207eea24c04c29af
Profesor <martinezjaraba@iestubalcain.net>
2025-09-12 12:26:26 +0200

    Ejercicio 1

---
+++ ejercicio1/src/01_variables.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val name = "Kotlin"
+    println("Hello, " + name + "!")
+    println("Hola mundo")
+    println("Hola clase")
+
+
+
+    for (i in 1..5) {
+        println("i = $i")
+    }
+}
